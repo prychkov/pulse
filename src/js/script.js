cfe0537277d0605966c807045a4b30a89cda950d
Pavel <rychkovpavel@yandex.kz>
2020-04-26 08:17:58 +0600

    Add different version for carousel

--- src/js/script.js
+++ src/js/script.js
@@ -1,9 +1,9 @@
-$(document).ready(function(){
+/* $(document).ready(function(){
     $('.carousel__inner').slick({
         speed: 1200,
-        /* adaptiveHeight: true, */
-        prevArrow: '<button type="button" class="slick-prev"><img src="../icons/arrow_left.svg"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="../icons/arrow_right.svg"></button>',
+        adaptiveHeight: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="icons/arrow_left.svg"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="icons/arrow_right.svg"></button>',
         responsive: [
             {
                 breakpoint: 992,
@@ -15,3 +15,21 @@ $(document).ready(function(){
         ]
       });
   });
+  выше использовался JQery и слик слайрер */
+
+const slider = tns({
+    container: '.carousel__inner',
+    items: 1,
+    slideBy: 'page',
+    autoplay: false,
+    controls: false,
+    nav: false,
+});
+
+document.querySelector('.prev').addEventListener('click', function () {
+    slider.goTo('prev');
+});
+
+document.querySelector('.next').addEventListener('click', function () {
+    slider.goTo('next');
+});
